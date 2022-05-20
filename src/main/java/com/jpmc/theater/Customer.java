a9048789ff67443b7a1654820f1080f073a557fb
Zev <czschwartz@gmail.com>
2022-05-20 16:35:07 -0400

    refactor Customer to use Java Record

--- src/main/java/com/jpmc/theater/Customer.java
+++ src/main/java/com/jpmc/theater/Customer.java
@@ -1,40 +1,4 @@
 package com.jpmc.theater;
 
-import org.jetbrains.annotations.NotNull;
-import java.util.Objects;
-
-public class Customer {
-
-    private String name;
-
-    private String id;
-
-    /**
-     * @param name customer name
-     * @param id customer id
-     */
-    public Customer(String name, String id) {
-        this.id = id; // NOTE - id is not used anywhere at the moment
-
-        this.name = name;
-
-        }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (!(o instanceof Customer)) return false;
-        Customer customer = (Customer) o;
-        return Objects.equals(name, customer.name) && Objects.equals(id, customer.id);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(name, id);
-    }
-
-    @Override
-    public @NotNull String toString() {
-        return "name: " + name;
-    }
+record Customer(String name, String id) {
 }
