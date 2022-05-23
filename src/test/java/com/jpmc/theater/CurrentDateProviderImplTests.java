8e8f2c8d53ab915fbf8a9dcae5f008369bc9a624
Zev <czschwartz@gmail.com>
2022-05-23 11:16:12 -0400

    remove unneeded test files and add test for CurrentDateProviderImpl

--- src/test/java/com/jpmc/theater/CurrentDateProviderImplTests.java
+++ src/test/java/com/jpmc/theater/CurrentDateProviderImplTests.java
@@ -1,10 +1,23 @@
 package com.jpmc.theater;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
 
 public class CurrentDateProviderImplTests {
     @Test
-    void makeSureCurrentTime() {
-        System.out.println("current time is - " + CurrentDateProviderImpl.getInstance().currentDate());
+    void shouldReturnCorrectDate() {
+        Assertions.assertEquals(LocalDate.now(),
+                CurrentDateProviderImpl.getInstance().currentDate(),
+                "Current date should be accurate");
+    }
+
+    @Test
+    void shouldReturnAlwaysSameInstance() {
+        var instance1 = CurrentDateProviderImpl.getInstance();
+        var instance2 = CurrentDateProviderImpl.getInstance();
+
+        Assertions.assertSame(instance1, instance2, "singleton should always return the same instance");
     }
 }
