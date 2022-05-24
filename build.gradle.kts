ffdd5931ab1a8e187ce0728560c36a63935c5b3d
Zev <czschwartz@gmail.com>
2022-05-24 00:46:34 -0400

    add moshi

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,9 +12,11 @@ repositories {
 
 var junitVersion = "5.8.2"
 var jetbrainsAnnation = "23.0.0"
+var moshiVersion = "1.13.0"
 
 dependencies {
     compileOnly("org.jetbrains:annotations:$jetbrainsAnnation")
+    implementation("com.squareup.moshi:moshi:$moshiVersion")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:$junitVersion")
     testImplementation("org.junit.jupiter:junit-jupiter-params:$junitVersion")
 }
